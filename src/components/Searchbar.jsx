0d1a84286d2f01722b81b80161c61ca5ec36555b
Samir1113 <94626926+Samir1113@users.noreply.github.com>
2024-02-17 19:44:25 +0200

    added Alerts Line

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -9,15 +9,15 @@ function Searchbar() {
     <>       
        <div className="row d-flex  align-items-center justify-content-between mb-4">
                 <div className="col-md-3">
-                  <h1 className="ms-4 ps-4 text-white">Alerts</h1>
+                  <h1 className={`ms-4 ps-4 text-white ${styles.alert}`}>Alerts</h1>
                 </div>
                 <div className="col-md-8">
                   <input className={`mx-auto w-100 text-start ${styles.search}`} type="text" placeholder="Search By..." style={{ color: "white" }} />
                 </div>
                 <div className="col-md-1">
                
-                <div className="position-relative">
-                    <IoIosNotifications style={{color:"cyan",fontSize:"30px"}}/>
+                <div className="position-relative ">
+                    <IoIosNotifications style={{color:"cyan",display:"inline-block",fontSize:"30px"}}/>
                      <div className={styles.notifyNumber}>6</div>
                 </div>
               </div>
